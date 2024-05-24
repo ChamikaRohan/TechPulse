5e6be3c2583b4599e2b1609d8fcf3c5b8c483577
ChamikaRohan <chamika707@gmail.com>
2024-05-24 11:43:39 +0530

    Combined delete-API controller with frontend

--- tech-frontend/src/pages/Homepage.jsx
+++ tech-frontend/src/pages/Homepage.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import PopUpForm from '../components/PopUpForm'
+import PopUpFormForCreate from '../components/PopUpFormForCreate'
 import ProductList from '../components/ProductList'
 
 export default function Homepage() {
@@ -7,7 +7,7 @@ export default function Homepage() {
     <div className='bg-slate-100 rounded-3xl p-8 transition-transform'>
       <h1 className='sm:text-lg md:text-3xl uppercase font-extrabold text-blue-700 lg:text-5xl'>Product Details</h1>
       <ProductList/>
-      <PopUpForm/>
+      <PopUpFormForCreate/>
     </div>
   )
 }
